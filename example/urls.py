16ca8f6586e35e390f683ad2ed6114647d33e703
Helior Colorado <me@helior.info>
2022-03-28 23:38:57 -0700

    doc: Areas of automation

--- example/urls.py
+++ example/urls.py
@@ -3,18 +3,22 @@ from django.urls import include, path
 from rest_framework import routers, serializers, viewsets
 from . import models
 
-
+# 🔁 Iterate over models
 class ExampleSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = models.Example
+    # 🔁 Iterate over none-hidden attributes
     fields = ['id', 'name', 'created_on', 'updated_on']
 
-
+# 🔁 Iterate over models
 class ExampleViewSet(viewsets.ModelViewSet):
   queryset = models.Example.objects.all()
   serializer_class = ExampleSerializer
 
+
 router = routers.DefaultRouter()
+
+# 🔁 Iterate over models
 router.register(r'examples', ExampleViewSet)
 
 urlpatterns = [
