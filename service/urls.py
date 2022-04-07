75b41a5e1a578b86a988a1742bc5027c5ba7bc6e
Helior Colorado <me@helior.info>
2022-04-06 18:35:57 -0700

    Swagger metadata update

--- service/urls.py
+++ service/urls.py
@@ -21,11 +21,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Django Clean",
       default_version='v1',
-      description="Test description",
+      description="Swagger docs for Django Clean!",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      contact=openapi.Contact(email="dev-null@helior.info"),
       license=openapi.License(name="BSD License"),
    ),
    public=False,
